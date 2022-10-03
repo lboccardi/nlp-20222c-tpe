02c7a027e00c0ecc8908b54fb6a290fa03467eb4
Luciano Boccardi <luciano.boccardi@mudafy.com>
2022-10-03 09:14:46 -0300

    added the missing preprocessing code as comment

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -10,6 +10,19 @@ def normalize(string_1, string_2 = ''):
     return [w.lower() for w in title_tokenized_words if not (w.lower() in stop_words) and (w.lower().isalnum())]
 
 if __name__ == "__main__":
+
+    ## This lines are just for generating the news.csv from Real and Fake datasets
+    
+    # df_fake: pd.DataFrame = pd.read_csv('datasets/Fake.csv', header=0)
+    # df_true: pd.DataFrame = pd.read_csv('datasets/True.csv', header=0)
+
+    # df_fake = df_fake.assign(truth=0)
+    # df_true = df_true.assign(truth=1)
+
+    # df_news = pd.concat([df_fake, df_true], ignore_index=True)
+
+    # df_news.to_csv('./datasets/news.csv', index=False)
+
     df: pd.DataFrame = pd.read_csv(PATH_TO_FILE)
     df['norm_title'] = df.apply(lambda row: normalize(row['title']), axis=1)
     df['norm_text'] = df.apply(lambda row: normalize(row['text']), axis=1)
