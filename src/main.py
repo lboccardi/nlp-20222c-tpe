917f7da0a9ac4e4a5386de8455dd83113fd3e0b2
lboccardi <lboccardi@itba.edu.ar>
2022-12-05 00:06:44 -0300

    added cross-validation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from ast import literal_eval
@@ -6,19 +7,14 @@ import os
 from wordcloud import WordCloud
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import make_pipeline
 from sklearn.metrics import accuracy_score, precision_score, confusion_matrix, ConfusionMatrixDisplay
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, train_test_split
 
 PATH_TO_FILE = "datasets/news_norm.csv"
 OUT_DIR = "out"
 TEXT_ATTRIBUTE = "norm_title"
-
-TRANSLATE = {
-    "multinomial_nb": "Multinomial Naive Bayes",
-    "bernoulli_nb": "Bernoulli Naive Bayes",
-}
 
 def generate_wordcloud(words, file_name = ''):
     wordcloud = WordCloud(
@@ -45,34 +41,37 @@ if __name__ == "__main__":
     df[TEXT_ATTRIBUTE] = df[TEXT_ATTRIBUTE].apply(literal_eval).apply(lambda x: ' '.join(x))
     all_words = ' '.join(df[TEXT_ATTRIBUTE])
 
+    model =  make_pipeline(TfidfVectorizer(), BernoulliNB())
+    accuracies = cross_val_score(model, df[TEXT_ATTRIBUTE], df['truth'], scoring='accuracy', cv=10)
+    precisions = cross_val_score(model, df[TEXT_ATTRIBUTE], df['truth'], scoring='precision', cv=10)
+
+    print(f"Mean accuracy value was: {accuracies.mean():.3f}")
+    print(f"Mean precision value was: {precisions.mean():.3f}")
+    
+    print(f"Max accuracy value was: {accuracies.max():.3f} on run {np.argmax(accuracies) + 1}" )
+    print(f"Max precision value was: {precisions.max():.3f} on run {np.argmax(precisions) + 1}")
+
     random_states = [42, 59, 81, 90, 156, 200, 215, 456, 9875, 1354]
 
-    classes = {
-        "multinomial_nb": MultinomialNB,
-        "bernoulli_nb": BernoulliNB
-    }
+    accuracy_array = []
+    precision_array = []
+    
+    for value in random_states:
+        X_train, X_test, y_train, y_test = train_test_split(df[TEXT_ATTRIBUTE], df['truth'], test_size=0.1, random_state=value)
 
-    for key in classes.keys():
-        accuracy_array = []
-        precision_array = []
-        
-        for value in random_states:
-            X_train, X_test, y_train, y_test = train_test_split(df[TEXT_ATTRIBUTE], df['truth'], test_size=0.1, random_state=value)
+        model.fit(X_train, y_train)
+        predicted_categories = model.predict(X_test)
 
-            model = make_pipeline(TfidfVectorizer(), classes[key]())
-            model.fit(X_train, y_train)
-            predicted_categories = model.predict(X_test)
+        accuracy_array.append(accuracy_score(y_test, predicted_categories))
+        precision_array.append(precision_score(y_test, predicted_categories))
+    
+    cm = confusion_matrix(y_test, predicted_categories, labels=model.classes_)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
+    disp.plot()
 
-            accuracy_array.append(accuracy_score(y_test, predicted_categories))
-            precision_array.append(precision_score(y_test, predicted_categories))
-        
-        cm = confusion_matrix(y_test, predicted_categories, labels=model.classes_)
-        disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=model.classes_)
-        disp.plot()
-
-        plt.savefig(f'{OUT_DIR}/confussion_matrix_{key}')
-        
-        fig, axis = plt.subplots()
-        axis.set_title(f'Accuracy and Precision with {TRANSLATE[key]}')
-        axis.boxplot([accuracy_array, precision_array],  labels=("Accuracy","Precision"))
-        plt.savefig(f'{OUT_DIR}/boxplot_{key}')
+    plt.savefig(f'{OUT_DIR}/confussion_matrix_bernoulli_nb')
+    
+    fig, axis = plt.subplots()
+    axis.set_title(f'Accuracy and Precision with Bernoulli NB')
+    axis.boxplot([accuracy_array, precision_array],  labels=("Accuracy","Precision"))
+    plt.savefig(f'{OUT_DIR}/boxplot_bernoulli_nb')
